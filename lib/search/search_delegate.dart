24624c94c01fed2b104dcea003c6c09c6c2aa3c6
Andres Mestra <andresmestra11a@gmail.com>
2022-04-12 21:23:34 -0500

    feat: implement delegate search

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:peliculas/models/models.dart';
+import 'package:peliculas/providers/movies_provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   @override
@@ -6,12 +9,22 @@ class MovieSearchDelegate extends SearchDelegate {
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [Text('BuildActions')];
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () => query = '',
+      ),
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return Text('BuildLeading');
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
   }
 
   @override
@@ -19,8 +32,52 @@ class MovieSearchDelegate extends SearchDelegate {
     return Text('BuildResults');
   }
 
+  Widget _emptyContainer() {
+    return const Center(
+      child: Icon(
+        Icons.movie_creation_outlined,
+        color: Colors.black38,
+        size: 130,
+      ),
+    );
+  }
+
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Text('BuildSuggestions: $query');
+    if (query.isEmpty) {
+      return _emptyContainer();
+    }
+
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+
+    return FutureBuilder(
+      future: moviesProvider.searchMovies(query),
+      builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
+        if (!snapshot.hasData) return _emptyContainer();
+
+        final movies = snapshot.data!;
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (_, int index) => _MovieItem(movie: movies[index]),
+        );
+      },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+  const _MovieItem({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FadeInImage(
+        placeholder: const AssetImage('assets/no-image.jpg'),
+        image: NetworkImage(movie.fullPosterImg),
+        width: 50,
+        fit: BoxFit.contain,
+      ),
+    );
   }
 }
