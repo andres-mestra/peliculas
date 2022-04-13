e37f73f1b27bed460ee169dd14c19bc8aeaab1de
Andres Mestra <andresmestra11a@gmail.com>
2022-04-12 21:03:19 -0500

    feat: define searchResponse and get movies

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -20,7 +20,7 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   Future<String> _getJsonData(String endpoint, [int page = 1]) async {
-    var url = Uri.https(_baseUrl, endpoint, {
+    final url = Uri.https(_baseUrl, endpoint, {
       'api_key': _apiKey,
       'language': _language,
       'page': '$page',
@@ -58,5 +58,18 @@ class MoviesProvider extends ChangeNotifier {
 
     moviesCast[movieId] = creditsResponse.cast;
     return creditsResponse.cast;
+  }
+
+  Future<List<Movie>> searchMovies(String query) async {
+    final url = Uri.https(_baseUrl, '/3/search/movie', {
+      'api_key': _apiKey,
+      'language': _language,
+      'query': query,
+    });
+
+    final response = await http.get(url);
+    final searchResponse = SearchResponse.fromJson(response.body);
+
+    return searchResponse.results;
   }
 }
